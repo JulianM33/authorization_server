95fc939a9b8a62af739f780ab558516460be98f7
Julian Meijerink <julian.meijerink@postnl.nl>
2023-09-02 15:16:13 +0200

    Add login and sign-up API endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Union
+from fastapi import FastAPI, Body
 
-from fastapi import FastAPI
+from app.schemas.api_schemas import UserSchema, UserLoginSchema
 
 app = FastAPI()
 
@@ -12,7 +12,23 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema = Body(default=None)):
+    users.append(user)
+    return {"User created"}
+
+
+@app.post("/user/login", tags=["user"])
+def login_user(user_data: UserLoginSchema = Body(default=None)):
+    for user in users:
+        if user.email == user_data.email:
+            if user.password != user_data.password:
+                return {"Error, wrong password!"}
+            else:
+                # Return signed token
+                return {"User logged in!"}
+    return {"Error, email not recognized! "}
+
+
+
 
